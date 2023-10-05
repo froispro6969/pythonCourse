751e87e341649c2090fbd06a499ab2e1ce707506
Damian <froispro6969@gmail.com>
2023-10-05 22:27:00 +0200

    variables

---
+++ multiple_assignment.py
@@ -0,0 +1,23 @@
+# name = "Fro"
+# age = 19
+# human = True
+
+name, age, human = "Fro", 19, True
+work = "IT"
+
+print(name)
+print(age)
+print(human)
+print(work)
+
+# Robert = 20
+# Janusz = 20
+# Iwona = 20
+# Andrzej = 20
+
+Robert = Janusz = Iwona = Andrzej = 20
+
+print(Robert)
+print(Janusz)
+print(Iwona)
+print(Andrzej)

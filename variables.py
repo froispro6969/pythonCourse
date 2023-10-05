e1513fa61dd4e63b8f90d90586fad2e2b4d13101
Damian <froispro6969@gmail.com>
2023-10-05 22:14:54 +0200

    variables

---
+++ variables.py
@@ -0,0 +1,22 @@
+# VARIABLES
+
+# first_name = "Damian"
+# last_name = "Fro"
+# name = first_name + " " + last_name
+
+# print("Hello " + name)
+# print(type(name))
+
+# age = 19
+# age += 1
+# print("Your age is"+ str(age))
+# print(type(age))
+
+# height = 193
+# print("Your height is: " + str(height) )
+# print(type(height))
+
+# animal = False
+# print(animal)
+# print(type(animal))
+

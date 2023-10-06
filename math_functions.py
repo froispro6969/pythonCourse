b287794ced85076fb06fa748572189d958a311a6
Damian <froispro6969@gmail.com>
2023-10-06 17:11:22 +0200

    math functions

---
+++ math_functions.py
@@ -0,0 +1,15 @@
+import math
+
+pi = -3.14
+x = 1
+y = 2
+z = 3
+
+#print(round(pi))
+#print(math.ceil(pi)) #zaokragla w gore
+#print(math.floor(pi)) #zaokragla w dol
+#print(abs(pi)) #wartosc bezwzgledna
+#print(pow(pi,2)) #do potegi
+#print(math.sqrt(420)) #pierwiastek
+#print(max(x,y,z))
+#print(min(x,y,z))

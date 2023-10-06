aec709f4f8b22013be58f771d13aa370b7bb6d06
Damian <froispro6969@gmail.com>
2023-10-06 16:50:09 +0200

    string methods

---
+++ string_methods.py
@@ -0,0 +1,12 @@
+name = "Damian fro"
+
+#print(len(name))
+#print(name.find("a"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha()) ## sprawdza czy string posiada TYLKO litery
+#print(name.count("o")) ## ile jest "o" w podanym stringu
+#print(name.replace("a","o"))
+#print(name*3)

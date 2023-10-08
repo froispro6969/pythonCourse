0764b7f0bb72a9c1a3d399d684d2058c6d8ced25
Damian <froispro6969@gmail.com>
2023-10-08 19:46:48 +0200

    string slicing

---
+++ string_slicing.py
@@ -0,0 +1,19 @@
+# name = "Damian Fro"
+
+# first_name = name[0:6] # or name[:6]
+# last_name = name[7:10] # or name[7:]
+# funky_name = name[0:10:2] # or name[::3] # co 2 litere
+# reversed_name = name[::-1]
+#
+# print(first_name)
+# print(last_name)
+# print(funky_name)
+# print(reversed_name)
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+
+slice = slice(7,-4)
+
+print(website1[slice])
+print(website2[slice])

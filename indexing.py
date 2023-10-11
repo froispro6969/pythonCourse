59e13d0af318819539dba828f015c38b03bac483
Damian <froispro6969@gmail.com>
2023-10-11 17:09:16 +0200

    indexing

---
+++ indexing.py
@@ -0,0 +1,12 @@
+name = "damian Froo!"
+
+#if name[0].islower():
+    #name = name.capitalize()
+
+first_name = name[:6].upper()
+last_name = name[7:].lower()
+last_char = name[-1]
+
+print(first_name)
+print(last_name)
+print(last_char)

8ef781cdcfeef893e52d8fa808995159b2e561ce
Damian <froispro6969@gmail.com>
2023-10-11 17:17:56 +0200

    function

---
+++ functions/function.py
@@ -0,0 +1,12 @@
+def hello(name, surname, age):
+    print("HELLO!!" + name + " " + surname)
+    print("You are "+str(age)+" years old")
+    print("Have a nice day")
+
+
+my_name = "Froo"
+#hello("Damian")
+#hello("Dude")
+#hello(my_name)
+
+hello("Damian", "Frooo", 21)

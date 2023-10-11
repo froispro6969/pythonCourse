045c90c764c2c43316d67f39a117a3e4a7490321
Damian <froispro6969@gmail.com>
2023-10-11 17:01:20 +0200

    dictionary

---
+++ lists/dictionary.py
@@ -0,0 +1,20 @@
+#dictionary = A changeable, unordered collection of unique key: value pairs. Fast because they use hashing, allow us to acces a value quickly.
+
+capitals = {"USA": "Washinghton",
+            "India": "New Dehli",
+            "China": "Beijing",
+            "Russia": "Moscow"}
+
+capitals.update({"Germany": "Berlin"})
+capitals.update({"USA":"Las Vegas"})
+capitals.pop("China")
+#capitals.clear()
+
+#print(capitals["Germany"])
+#print(capitals.get("Germany"))
+#print(capitals.keys())
+#print(capitals.values())
+#print(capitals.items()) # wyswietla caly slownik
+
+for key,value in capitals.items():
+    print(key, value)

9c0325fa682338a7616565a5a1a91d84b29f627f
Damian <froispro6969@gmail.com>
2023-10-12 15:27:01 +0200

    kwargs

---
+++ functions/kwargs.py
@@ -0,0 +1,9 @@
+#** kwargs = parameter that will pack all arguments into a dictionary.
+
+def hello(**kwargs):
+    print("Hello " + kwargs['first'] + " "+ kwargs['last'])
+    print("Hello", end=" ")
+    for key,value in kwargs.items():
+        print(value, end=" ")
+
+hello(title="Mr.",first="Damian",middle="Froo", last="asdasd")

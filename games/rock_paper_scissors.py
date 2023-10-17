ffe61f5193806778a155d7e04715ae481d04ae03
Damian <froispro6969@gmail.com>
2023-10-17 18:16:38 +0200

    rock, paper, scissors

---
+++ games/rock_paper_scissors.py
@@ -0,0 +1,46 @@
+import random
+
+
+while True:
+    choices = ["rock", "paper", "scissors"]
+    computer = random.choice(choices)
+    player = None
+
+    while player not in choices:
+        player = input("rock, paper or scissors?: ").lower()
+
+    if player == computer:
+        print("computer: ", computer)
+        print("player: ", player)
+        print("DRAW!")
+    elif player == "rock":
+        if computer == "paper":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You lose!")
+        if computer == "scissors":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You win!")
+    elif player == "paper":
+        if computer == "rock":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You win")
+        if computer == "scissors":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You lose!")
+    elif player == "scissors":
+        if computer == "rock":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You lose!")
+        if computer == "paper":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You win!")
+    play_again = input("Do u wanna to play again?: ")
+    if play_again != "yes":
+        break
+

bc5a78998717e0658844ebb35b67dddd92952563
Damian <froispro6969@gmail.com>
2023-10-26 18:38:05 +0200

    if__name__=="__main__"

--- if_name_main.py
+++ if_name_main.py
@@ -1,3 +1,10 @@
+#1. Module can be run as a standolne program or
+#2. Module can imported and used by other modules.
+
+# Python interpreter sets "special variables", one of which is __name__.
+# Python will assign the __name__ variable a value of "__main__" if it's the initial module being run.
+
+
 
 def hello():
     print("Hello")
